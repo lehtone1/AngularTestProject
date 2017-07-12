36d6fb25624f0850044807e78a68c7f9d4f94fa7
Eero Lehtonen <eero.lehtonen@aalto.fi>
2017-07-12 19:44:12 +0300

    Modifying receipe and receipe HTML content, and creating recipe model

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'test', 'https://www.google.fi/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiG-cGtlYTVAhVlDJoKHaWHBrgQjRwIBw&url=http%3A%2F%2Fwww.seriouseats.com%2F2017%2F03%2Fpressure-cooker-recipes.html&psig=AFQjCNGiCOzRnp5qob3ocKXxEnR_lIX7Cg&ust=1499963429731319')
+  ];
 
   constructor() { }
 
